df2ac16eec2ca3457d887fd4d2cf0d7cfe8bc27b
MishaelChL <jose.mishael.chile@gmail.com>
2021-07-13 17:10:55 -0400

    Proyecto Web Completo XI, moviendo urls y templates a su debido app, en este caso a servicio todo lo q se le relaciones

--- Proyectowebapp/views.py
+++ Proyectowebapp/views.py
@@ -1,13 +1,8 @@
 from django.shortcuts import render, HttpResponse
-from servicios.models import Servicio
 # Create your views here.
 
 def home(request):
     return render(request, "Proyectowebapp/home.html")
-
-def servicios(request):
-    servicios = Servicio.objects.all()
-    return render(request, "Proyectowebapp/servicios.html", {'servicios': servicios})
 
 def tienda(request):
     return render(request, "Proyectowebapp/tienda.html")
