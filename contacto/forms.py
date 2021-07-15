a5e033e769fd34e52104aacdd92e89421fb2d360
MishaelChL <jose.mishael.chile@gmail.com>
2021-07-15 09:01:09 -0400

    Proyecto Web Completo XIX, terminando el formulario de contacto

--- contacto/forms.py
+++ contacto/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class FormularioContacto(forms.Form):
     nombre = forms.CharField(label="Nombre", required=True)
-    email = forms.CharField(label="Email", required=True)
-    contenido = forms.CharField(label="Contenido")
+    email = forms.EmailField(label="Email", required=True)
+    contenido = forms.CharField(label="Contenido", widget=forms.Textarea)
